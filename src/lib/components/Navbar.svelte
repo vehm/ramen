743dbe1c4cec80f5978a1064d213529aef88db88
vehm <services@ramen.dev>
2024-12-08 22:36:38 -0600

    added mini logo in navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	import RamenMiniLogo from '$lib/assets/ramen_mini.svg';
+</script>
+
 <header class="flex justify-center p-4">
-	<a href="/" class="flex gap-0 rounded-full p-2 transition-colors hover:bg-[#10182B]">
+	<a href="/" class="flex items-center gap-2 rounded-full p-2 transition-colors hover:bg-[#10182B]">
+		<img src={RamenMiniLogo} alt="Ramen Logo" class="h-6 w-6" />
 		<h1 class="text-center text-xl font-bold text-white">
 			<span class="text-[#FA6E81]">ramen</span>.dev
 		</h1>
