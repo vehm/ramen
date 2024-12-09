ef1bf14a889b3eb7b6c962a2136764a310b4f216
vehm <services@ramen.dev>
2024-12-08 22:52:49 -0600

    changed flow on small screens

--- src/lib/components/WelcomeCard.svelte
+++ src/lib/components/WelcomeCard.svelte
@@ -5,7 +5,9 @@
 </script>
 
 <div class="flex flex-col gap-2 px-4 text-center md:text-start">
-	<h1 class="flex items-end gap-4 text-4xl font-light text-white">
+	<h1
+		class="flex flex-col items-center gap-4 text-4xl font-light text-white md:flex-row md:items-end"
+	>
 		<img src={RamenMiniLogo} alt="Ramen Logo" class="h-12 w-12" />
 		<span>
 			Hi there, I'm <TextSlideshow
