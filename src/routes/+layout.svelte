213ea48aaa62db533dc85cca9da9142e91af9014
vehm <services@ramen.dev>
2024-12-08 21:33:49 -0600

    added inter font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,22 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<div class="bg-[#090D15]">
+	{@render children()}
+</div>
+
+<style>
+	:global(body) {
+		font-family: 'Inter', sans-serif;
+		font-optical-sizing: auto;
+	}
+</style>
