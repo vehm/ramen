a61f56a55a936a50916981d17ef27259684ae7d6
vehm <services@ramen.dev>
2024-12-08 21:35:11 -0600

    created basic chat view ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { useChat } from '@ai-sdk/svelte';
+
+	import { ChatForm, ChatMessageList, WelcomeCard } from '$lib/components';
+
+	const { input, handleSubmit, messages, isLoading } = useChat();
+</script>
+
+<main class="h-dvh max-h-dvh overflow-auto">
+	<div class="flex h-full items-center justify-center">
+		<div class="flex h-full w-full max-w-3xl flex-col justify-center gap-4 pb-4">
+			{#if $messages.length === 0}
+				<WelcomeCard />
+			{:else}
+				<div class="p-4 text-center font-bold text-white">Testing</div>
+				<ChatMessageList messages={$messages} />
+			{/if}
+			<ChatForm bind:input={$input} {handleSubmit} isLoading={$isLoading} />
+		</div>
+	</div>
+</main>
