d7740a6faf663eb76e7de6ff4f58b0a0a4f7485b
vehm <services@ramen.dev>
2024-12-09 22:06:47 -0600

    added icons for message senders

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -2,23 +2,39 @@
 	import { slide } from 'svelte/transition';
 	import { marked } from 'marked';
 
+	import { User } from 'lucide-svelte';
+
+	import RamenMini from '$lib/assets/ramen_mini.svg';
+
 	type Props = {
 		role: string;
 		content: string;
 	};
 
 	let { role, content }: Props = $props();
+
+	let isUser = $derived(role === 'user');
 </script>
 
 <li class="flex gap-2" transition:slide>
 	<div class="flex flex-col gap-2">
-		<span
-			class="font-bold capitalize"
-			class:text-[#8AA1A8]={role === 'user'}
-			class:text-[#FA6E81]={role === 'assistant'}
+		<!-- No icon sender option -->
+		<!-- <span
+			class="w-min text-nowrap text-xs uppercase {isUser ? 'text-[#8AA1A8]' : 'text-[#FA6E81]'}"
 		>
-			{role}
-		</span>
+			{isUser ? 'You' : 'Ramen'}
+		</span> -->
+		{#if isUser}
+			<span class="flex items-center gap-2">
+				<User class="h-6 w-6 rounded bg-[#8AA1A8] p-1 text-[#090d15]" />
+				<span class="w-min text-nowrap text-xs uppercase text-[#8AA1A8]">You</span>
+			</span>
+		{:else}
+			<span class="flex items-center gap-2">
+				<img src={RamenMini} alt="Ramen Logo" class="h-6 w-6 rounded" />
+				<span class="w-min text-nowrap text-xs uppercase text-[#FA6E81]">Ramen</span>
+			</span>
+		{/if}
 		<p class="prose prose-invert text-white">{@html marked.parse(content)}</p>
 	</div>
 </li>
