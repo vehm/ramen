32594cc6d8563cc7da527da8a62fd02c17ec7b3a
vehm <services@ramen.dev>
2024-12-10 19:58:23 -0600

    added local inter font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
 	import '../app.css';
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-		rel="stylesheet"
-	/>
+	<link rel="stylesheet" href="/fonts/inter/inter.css" />
 </svelte:head>
 
 {@render children()}
-
-<style>
-	:global(body) {
-		font-family: 'Inter', sans-serif;
-		font-optical-sizing: auto;
-		background-color: #090d15;
-	}
-</style>
